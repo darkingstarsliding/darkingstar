08a8f0878caad3e227289f23d1593949b533f886
darking <2380290809@qq.com>
2026-03-28 23:14:41 +0800

    third day

---
+++ game.py
@@ -0,0 +1,10 @@
+﻿import pygame
+from module.logger import MyLogger
+from public import globvalue
+
+if __name__=='__main__':
+    logger=MyLogger(__name__)
+    globvalue.logger=logger
+    
+    #pygame init
+    pg=pygame.init()
